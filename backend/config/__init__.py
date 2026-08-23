b39d0436db38c9eb2700e31f6218dd517ccd2dfc
maneeshanif <manees7865@gmail.com>
2026-08-24 01:11:05 +0500

    chore: untrack and ignore old backend folder to keep it local-only

--- backend/config/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Configuration exports for the Facebook integration."""
-
-from .fb_config import FacebookConfig, get_config, load_config
-
-__all__ = [
-    "FacebookConfig",
-    "get_config",
-    "load_config",
-]
--- backend/exceptions.py
+++
@@ -1,3 +0,0 @@
-"""Compatibility shim. Prefer importing from :mod:`src.exceptions`."""
-
-from src.exceptions import *  # noqa: F401,F403
